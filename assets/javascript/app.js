55a6d73886483a32f412619a0564091739532a16
mohw <michaelohw5@gmail.com>
2018-12-09 15:51:12 -0800

    mostly working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,73 +1,92 @@
-var buttonDiv = $("#button-div");
+
+var buttonDiv = $("#buttons");
+var addBtn = $("#add-topic");
+var topicInput = $("#topic-input");
 var topics = ["cat", "dog", "kangaroo", "trebuchet"];
-
-var topic = "";
-var queryURL = "";
-var gifArr = [];
 var limit = 10;
+var clicked = "";
 
 
 
+//make buttons by going through topics array
 var renderButtons = function () {
     //empty the buttons-div first
     buttonDiv.empty();
     //create button for all the elements in topics array, append to button-div
     for (var i = 0; i < topics.length; i++) {
-        var newBtn = $("<button/>");
+        var newDiv = $("<div class='newImgDiv col-md-2'>")
+        var newBtn = $("<a>");
         newBtn.text(topics[i]);
-        newtopic = topics[i];
-        buttonDiv.append(newBtn);
+        var newTopic = topics[i];
         newBtn.attr({
-            "id": "gifButton-" + i,
-            "onClick": `loadGif(${newtopic})`,
-
+            id: "btn-" + i,
+            "data-gif": newTopic,
+            "class": "gif-btn btn btn-primary",
         });
-        $("#buttons").append(newBtn);
+        newDiv.append(newBtn);
+        buttonDiv.append(newDiv);
     }
 }
 
-var loadGif = function (topic) {
-    $("#gifs").empty();
-    ajaxFunc(topic);
+//empty the buttonDiv to make sure not to make duplicate buttons
+//function to get user input and add to topics by addTopic(), then render the buttons again.
+addBtn.on("click", function (event) {
+    event.preventDefault();
+    buttonDiv.empty();
+    addTopic();
+    renderButtons();
+});
+
+//gets the input value of the user to add into the topics array.
+var addTopic = function () {
+    var topicToAdd = topicInput.val();
+    topics.push(topicToAdd);
 }
 
-var ajaxFunc = function (topic) {
+//function to get gif using data attribute
+$(document.body).on("click", ".gif-btn", function (event) {
+    event.preventDefault();
+    clicked = $(this).attr("data-gif");
+    getGif(clicked);
+});
+
+var getGif = function(gif) {
+    $("#gifs").empty();
+    var apikey = "dc6zaTOxFJmzC";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=" + apikey + "&limit=" + limit;
     $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?q=" +
-            topic + "&api_key=dc6zaTOxFJmzC&limit=" + limit,
+        url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        for (var i = 0; i < response.data.length; i++) {
-            // var gifObj = response.data[i];
-            var gifId = response.data[i].id;
-            var gifUrl = response.data[i].images.original.url;
-            var gifRating = response.data[i].rating;
-            // gifObj.attr({
-            //     "id": gifId,
-            //     "url": gifUrl,
-            //     "rating": gifRating,
-            // });
-            gifArr.push({
-                id: gifId,
-                url: gifUrl,
-                rating: gifRating,
-            });
-            console.log(gifArr);
-        } 
-        appendGif();
-    });   
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div class='item col-md-3'>");
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            var image = $("<img>");
+            image.attr("src", results[i].images.fixed_height.url);
+            gifDiv.append(p, image);
+            $("#gifs").prepend(gifDiv);
+        }
+    });
 }
 
-var appendGif = function () {
-    console.log("Appendgif running");
-    for (var i=0; i<gifArr.length; i++) {
-        var image = $("<img>");
-        image.attr("src", gifArr[i].url);
-        $("#gifs").append(image);   
-        console.log("yo");
-    }
-}
+$("#loadMore").on("click", function() {
+    limit = limit + 10;
+    getGif(clicked);
+});
+
+$("#reset").on("click", function() {
+    limit = 10;
+    clicked = "";
+    $("#gifs").empty();
+})
 
 
 renderButtons();
+
+
+var test = function () {
+    console.log("test");
+}
+
